d8c64dff7dbf5624bbf291c59ad0c4bcf8d9d1b9
Artur <chokoplayshadow@gmail.com>
2025-12-26 16:43:25 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,26 +1,21 @@
-// firebase-config.js
+// Firebase Configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Імпорт Firebase SDK (версія 11.0.2)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
-
-// Конфігурація Firebase
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDdqDJC6iI2pXLtxm61220CIPgG4YdTrYk",
     authDomain: "inboxprodemo.firebaseapp.com",
     projectId: "inboxprodemo",
-    storageBucket: "inboxprodemo.firebasestorage.app",
-    messagingSenderId: "674512041878",
-    appId: "1:674512041878:web:d6c4f43b74c3fb5d6c4fb7",
-    measurementId: "G-DN859E1Y0L"
+    storageBucket: "inboxprodemo.appspot.com",
+    messagingSenderId: "1064944508829",
+    appId: "1:1064944508829:web:cb456d0301de25bdee4a81"
 };
 
-// Ініціалізація Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Експорт сервісів
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db, app };
+export { auth, db };
